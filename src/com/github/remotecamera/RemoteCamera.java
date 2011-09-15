c0b22c88efa2de9c6a383a104c1b1291b818bc3f
Christian <schick.ch@googlemail.com>
2011-09-15 13:11:24 +0200

    Initialized repository.

---
+++ src/com/github/remotecamera/RemoteCamera.java
@@ -0,0 +1,15 @@
+package com.github.remotecamera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RemoteCamera extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
