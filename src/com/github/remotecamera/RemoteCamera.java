6a1a398ed361a78ba87f61ccdea3f77e8b4b5168
Christian <schick.ch@googlemail.com>
2011-09-15 13:56:55 +0200

    Main activity implements SurfaceHolder.Callback.

--- src/com/github/remotecamera/RemoteCamera.java
+++ src/com/github/remotecamera/RemoteCamera.java
@@ -2,14 +2,22 @@ package com.github.remotecamera;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
 
-public class RemoteCamera extends Activity
-{
+public class RemoteCamera extends Activity implements SurfaceHolder.Callback {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+  public void surfaceCreated(SurfaceHolder arg0) {
+  }
+
+  public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+  }
+
+  public void surfaceDestroyed(SurfaceHolder arg0) {
+  }
 }
