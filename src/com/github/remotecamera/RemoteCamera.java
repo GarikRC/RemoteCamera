75c6ac39e366408ef66d1d563c6f0390ad143cf9
Christian <schick.ch@googlemail.com>
2011-09-15 14:19:40 +0200

    Implemented test code for taking pictures.

--- src/com/github/remotecamera/RemoteCamera.java
+++ src/com/github/remotecamera/RemoteCamera.java
@@ -1,23 +1,40 @@
 package com.github.remotecamera;
 
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
 
-public class RemoteCamera extends Activity implements SurfaceHolder.Callback {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+public class RemoteCamera extends Activity {
+
+  private Camera camera = null;
+  private Camera.PictureCallback pictureCallback = null;
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.main);
+
+      try {
+        camera = Camera.open();
+        pictureCallback = new Camera.PictureCallback() {
+          public void onPictureTaken(byte[] imageTaken, Camera camera) {
+            System.out.println("asljkfalsdjkfaslkdjf");
+          }
+        };
+        camera.takePicture(null, null, pictureCallback);
+      } catch ( Exception ex ) {
+      }
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+
+    if (camera != null) {
+      camera.release();
     }
-
-  public void surfaceCreated(SurfaceHolder arg0) {
   }
 
-  public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
-  }
-
-  public void surfaceDestroyed(SurfaceHolder arg0) {
-  }
+    
 }
